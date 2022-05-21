03a8bedd0b18060439f02155a597b542e5806a4e
netjava90 <saymak656@gmail.com>
2022-05-20 21:18:31 -0400

    testanagram

--- src/string/problems/UnitTestingStringProblem.java
+++ src/string/problems/UnitTestingStringProblem.java
@@ -1,8 +1,22 @@
 package string.problems;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class UnitTestingStringProblem {
-    public static void main(String[] args) {
+    //public static void main(String[] args) {
         //Apply Unit Test into all the methods in this package.
 
+    @Test
+    public void testanagram() {
+        boolean actualResult = Anagram.isAnagram("cat", "tac");
+        Assert.assertNotEquals( false, actualResult);
+        System.out.println("test passed");
     }
-}
+
+    }
+
